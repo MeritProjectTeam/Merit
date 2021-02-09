ca80f2a1bcaf535cc061cc1ec60e046916b9fd40
Xehlwan <35033267+Xehlwan@users.noreply.github.com>
2021-02-09 15:17:18 +0100

    Första ukast för webformulär med personlig profilinfo. Början på ett interface för profilservice.

---
+++ src/Merit/Merit.ProfileService/MockProfileService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Merit.ProfileService
+{
+    public class MockProfileService : IProfileService
+    {
+        private Person currentPerson;
+
+        public void SavePerson(int Id, string firstName, string lastName, DateTime dateOfBirth)
+        {
+            currentPerson = new(firstName, lastName, dateOfBirth);
+        }
+
+        public void SaveAddress(string street, string zipcode, string city)
+        {
+            return;
+        }
+
+        public void SaveContactInfo(string email, string phone)
+        {
+            return;
+        }
+
+        public int GetAgeFor(string firstName, string lastName)
+        {
+            return 45;
+        }
+    }
+}
