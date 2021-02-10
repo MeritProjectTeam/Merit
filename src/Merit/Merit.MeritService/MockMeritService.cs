0f32968f93f73e85c3a0a81a80952aa32636c3f7
janmikaelberglund <71063732+janmikaelberglund@users.noreply.github.com>
2021-02-10 14:17:27 +0100

    Update MockMeritService.cs
    Added Merit Service Methods

--- src/Merit/Merit.MeritService/MockMeritService.cs
+++ src/Merit/Merit.MeritService/MockMeritService.cs
@@ -8,7 +8,19 @@ namespace Merit.MeritService
     {
         public void SaveMerit(NewMerit merit)
         {
-            return;
+            using StreamWriter sw = new StreamWriter(/src/Merit/Merit.MeritService/Datafile/MeritMockar.csv, true)
+            {
+                sw.WriteLine($"{merit.Category},{merit.SubCategory},{merit.Description},{merit.Duration}");
+            }
+        }
+        public void ReadMerit()
+        {
+            string[] textFileLines = File.ReadAllLines(/src/Merit/Merit.MeritService/Datafile/MeritMockar.csv);
+            foreach (var line in textFileLines)
+	        {
+                string[] tempfile = line.Split(',');
+                NewMerit merit = new NewMerit(tempfile[0], tempfile[1], tempfile[2], tempfile[3]);
+	        }
         }
     }
 }
