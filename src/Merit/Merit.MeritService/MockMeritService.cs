694564ae01fd0096d7d5f880dface6a7d063fd27
PivoKrtek <71064195+PivoKrtek@users.noreply.github.com>
2021-02-10 11:23:16 +0100

    Created a page for adding a merit.

---
+++ src/Merit/Merit.MeritService/MockMeritService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merit.MeritService
+{
+    public class MockMeritService : IMeritService
+    {
+        public void SaveMerit(NewMerit merit)
+        {
+            return;
+        }
+    }
+}
---
+++ src/Merit/Merit.MeritService/NewMerit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Merit.MeritService
+{
+    public class NewMerit
+    {
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+    }
+}
