743a67a7066e025222a11929b6c99d1c925d6a21
JoaHol85 <71064236+JoaHol85@users.noreply.github.com>
2021-02-10 14:08:39 +0100

    Created 2 methods to use file as database.

--- src/Merit/Merit.MeritService/MockMeritService.cs
+++ src/Merit/Merit.MeritService/MockMeritService.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Merit.MeritService.
 
 namespace Merit.MeritService
 {
     public class MockMeritService : IMeritService
     {
+                
         public void SaveMerit(NewMerit merit)
         {
-            return;
+            using StreamWriter sw = new StreamWriter(/src/Merit/Merit.MeritService/Datafile/MeritMockar.csv, true)
+            {
+                sw.WriteLine($"{merit.Category},{merit.SubCategory},{merit.Description},{merit.Duration}");
+            }
+        }
+        public void ReadMerit()
+        {
+            string[] textFileLines = File.ReadAllLines(/src/Merit/Merit.MeritService/Datafile/MeritMockar.csv);
+            foreach (var line in textFileLines)
+	        {
+                string[] tempfile = line.Split(',');
+                NewMerit merit = new NewMerit(tempfile[0], tempfile[1], tempfile[2], tempfile[3]);
+	        }
         }
     }
 }
