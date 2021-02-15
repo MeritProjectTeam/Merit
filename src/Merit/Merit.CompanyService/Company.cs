5ef91e3be3ab096e0eff08467178924ac26682e5
janmikaelberglund <71063732+janmikaelberglund@users.noreply.github.com>
2021-02-15 11:48:22 +0100

    Created basic company profile page Created basic company service library

---
+++ src/Merit/Merit.CompanyService/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merit.CompanyService
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string OrgNumber { get; set; }
+        public string ContactName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+
+    }
+}
+    
---
+++ src/Merit/Merit.Web/Pages/Company.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Merit.CompanyService;
+
+namespace Merit.Web.Pages
+{
+    public class CompanyModel : PageModel
+    {
+        private readonly ICompanyService companyService = new CompanyService.CompanyService();
+
+        [BindProperty]
+        public string CompanyName { get; set; }
+        [BindProperty]
+        public string OrgNumber { get; set; }
+        [BindProperty]
+        public string ContactPerson { get; set; }
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Phone { get; set; }
+        [BindProperty]
+        public string Street { get; set; }
+        [BindProperty]
+        public string Zipcode { get; set; }
+        [BindProperty]
+        public string City { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            companyService.SaveCompany(CompanyName, OrgNumber);
+            companyService.SaveContactPerson(ContactPerson, Email, Phone);
+            companyService.SaveAdress(Street, Zipcode, City);
+        }
+    }
+}
