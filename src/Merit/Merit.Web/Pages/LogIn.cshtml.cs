5e5c47f738825365c0d0974c7d8f78ec05b17e4f
janmikaelberglund <71063732+janmikaelberglund@users.noreply.github.com>
2021-02-16 13:20:10 +0100

    Created Login page Created Registration page Created Service behind login/registration

--- src/Merit/Merit.Web/Pages/LogIn.cshtml.cs
+++ src/Merit/Merit.Web/Pages/LogIn.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AccountLibraryService;
 
 namespace Merit.Web.Pages
 {
     public class LogInModel : PageModel
     {
+
+        [BindProperty]
+        public User UserLogin { get; set; }
+
+        private readonly IAccount Account = new Account();
+
+        [BindProperty]
+        public string LoginMessage { get; set; }
+
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+            if (Account.CheckLogin(UserLogin))
+            {
+                LoginMessage = "Inloggningen lyckades!";
+            }
+            else
+            {
+                LoginMessage = "Felaktigt användarnamn eller lösenord";
+            }
+        }
+
     }
 }
---
+++ src/Merit/Merit.Web/Pages/Register.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AccountLibraryService;
+
+
+namespace LoginWebTesting.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public User NewAccount { get; set; }
+
+        [BindProperty]
+        public string RegisterMessage { get; set; }
+        private readonly IAccount account = new Account();
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+
+            if (NewAccount.Password == NewAccount.PasswordCheck)
+            {
+                switch (account.CheckExistingAccount(NewAccount))
+                {
+                    case 100:
+                        account.AddAccount(NewAccount);
+                        RegisterMessage = "Registreringen lyckades!";
+                        break;
+                    case 101:
+                        RegisterMessage = "Användarnamnet upptaget.";
+                        break;
+                    case 102:
+                        RegisterMessage = "Epost-adressen finns redan registrerad.";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                RegisterMessage = "Lösenorden stämmer inte överens.";
+            }
+        }
+
+
+    }
+}
