90ef29f4c95d4206799721841239ecad859479f2
Sebastian <seb.johansson@outlook.com>
2021-02-16 13:36:35 +0100

    Added Interfaces and Services.
    DB functioning

---
+++ src/Merit/Merit.Data/Interfaces/IProfileInfoService.cs
@@ -0,0 +1,16 @@
+﻿using Merit.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merit.Data.Interfaces
+{
+    public interface IProfileInfoService
+    {
+        PersonalInfo Get(int id);
+        public PersonalInfo EditPerson(PersonalInfo person);
+        List<PersonalInfo> GetAll();
+    }
+}
--- src/Merit/Merit.Web/Pages/Profile.cshtml.cs
+++ src/Merit/Merit.Web/Pages/Profile.cshtml.cs
@@ -5,43 +5,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Merit.ProfileService;
+using Merit.Data.Interfaces;
+using Merit.Data.DbServices;
 
 namespace Merit.Web.Pages
 {
     public class ProfileModel : PageModel
     {
-        private readonly IProfileService profileService = new MockProfileService();
+        //private readonly IProfileService profileService = new MockProfileService();
+        IMeritDbService dbService = new Services();
 
         [BindProperty]
-        public string FirstName { get; set; }
-        [BindProperty]
-        public string LastName { get; set; }
-        [BindProperty]
-        public string Email { get; set; }
-        [BindProperty]
-        public string Street { get; set; }
-        [BindProperty]
-        public string Zipcode { get; set; }
-        [BindProperty]
-        public string City { get; set; }
-        [BindProperty]
-        public string Phone { get; set; }
-        [BindProperty]
-        public DateTime DateOfBirth { get; set; }
+        public Data.Models.PersonalInfo APerson { get; set; }
 
-        public int Age { get; set; }
+
+        //[BindProperty]
+        //public string FirstName { get; set; }
+        //[BindProperty]
+        //public string LastName { get; set; }
+        //[BindProperty]
+        //public string Email { get; set; }
+        //[BindProperty]
+        //public string Street { get; set; }
+        //[BindProperty]
+        //public string Zipcode { get; set; }
+        //[BindProperty]
+        //public string City { get; set; }
+        //[BindProperty]
+        //public string Phone { get; set; }
+        //[BindProperty]
+        //public DateTime DateOfBirth { get; set; }
+
+        //public int Age { get; set; }
 
         public void OnGet()
         {
-
+            //dbService.SavePersonProfile();
         }
 
         public void OnPost()
         {
-            profileService.SavePerson(FirstName, LastName, DateOfBirth);
-            profileService.SaveAddress(Street, Zipcode, City);
-            profileService.SaveContactInfo(Email, Phone);
-            Age = profileService.GetAgeFor(FirstName, LastName);
+            dbService.SavePersonProfile(APerson);
+            //profileService.SavePerson(FirstName, LastName, DateOfBirth);
+            //profileService.SaveAddress(Street, Zipcode, City);
+            //profileService.SaveContactInfo(Email, Phone);
+            //Age = profileService.GetAgeFor(FirstName, LastName);
         }
     }
 }
