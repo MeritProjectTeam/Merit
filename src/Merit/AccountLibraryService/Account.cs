fd8c700e2550cfa72a87c72c25facf292b081135
janmikaelberglund <71063732+janmikaelberglund@users.noreply.github.com>
2021-02-16 13:51:55 +0100

    Fixed bugs with account library

---
+++ src/Merit/AccountLibraryService/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AccountLibraryService
+{
+    public class Account : IAccount
+    {
+        public void AddAccount(User user)
+        {
+            using StreamWriter sw = new StreamWriter("wwwroot/Datafile/MeritAccountMockar.csv", true);
+            sw.WriteLine($"{user.UserName},{user.Email},{EncryptPassword(user.Password)}");
+        }
+
+        public int CheckExistingAccount(User user)
+        {
+            string input;
+            using StreamReader sr = new StreamReader("wwwroot/Datafile/MeritAccountMockar.csv");
+            while ((input = sr.ReadLine()) != null)
+            {
+                string[] splitInput = input.Split(",");
+                if (splitInput[0].ToLower() == user.UserName.ToLower())
+                {
+                    return 101;
+                }
+                if (splitInput[1].ToLower() == user.Email.ToLower())
+                {
+                    return 102;
+                }
+            }
+            return 100;
+        }
+
+        public static string EncryptPassword(string password)
+        {
+            MD5 mD5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(password);
+            byte[] hash = mD5.ComputeHash(inputBytes);
+
+            string result = "";
+
+            foreach (var h in hash)
+            {
+                result += h.ToString("X2");
+            }
+
+            return result;
+        }
+
+        public bool CheckLogin(User user)
+        {
+            string input;
+            using StreamReader sr = new StreamReader("wwwroot/Datafile/MeritAccountMockar.csv");
+            while ((input = sr.ReadLine()) != null)
+            {
+                string[] splitInput = input.Split(",");
+                if (splitInput[0].ToLower() == user.UserName.ToLower() && splitInput[2] == EncryptPassword(user.Password))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Merit/AccountLibraryService/IAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AccountLibraryService
+{
+    public interface IAccount
+    {
+        public void AddAccount(User user);
+        public int CheckExistingAccount(User user);
+        public bool CheckLogin(User user);
+    }
+}
