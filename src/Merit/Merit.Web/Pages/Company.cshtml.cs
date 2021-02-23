bddb9277edf9cfc2e2ac75ba5919a872f6ed97f2
JoaHol85 <71064236+JoaHol85@users.noreply.github.com>
2021-02-23 14:06:08 +0100

    done?

--- src/Merit/Merit.Web/Pages/Company.cshtml.cs
+++ src/Merit/Merit.Web/Pages/Company.cshtml.cs
@@ -24,7 +24,7 @@ namespace Merit.Web.Pages
 
         public void OnPost()
         {
-            company.SaveCompany(ACompany);
+            company.SaveCompanyInfo(ACompany);
         }
     }
 }
