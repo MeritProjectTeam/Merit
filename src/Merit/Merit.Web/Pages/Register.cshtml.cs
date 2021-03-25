baa57693bcde802b44d7f6be1fc8e13eee28c1f0
Jimmy Borin <35033267+Xehlwan@users.noreply.github.com>
2021-03-25 06:43:16 +0100

    Register page model redone.

--- src/Merit/Merit.Web/Pages/Register.cshtml.cs
+++ src/Merit/Merit.Web/Pages/Register.cshtml.cs
@@ -11,6 +11,10 @@ using Merit.CompanyService;
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
 
 namespace LoginWebTesting.Pages
 {
@@ -18,14 +22,16 @@ namespace LoginWebTesting.Pages
     {
         private readonly SignInManager<IdentityUser> signInManager;
         private readonly UserManager<IdentityUser> userManager;
+        private readonly IEmailSender emailSender;
 
         [BindProperty]
         InputModel Input { get; set; }
 
-        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.emailSender = emailSender;
         }
 
         public void OnGetAsync()
@@ -33,11 +39,42 @@ namespace LoginWebTesting.Pages
 
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            returnUrl ??= Url.Content("~/");
+
             if (ModelState.IsValid)
             {
+                var user = new IdentityUser { Email = Input.Email, UserName = Input.Email };
+                var result = await userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    if (userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                        var callbackUrl = Url.Page(
+                            "/Account/ConfirmEmail",
+                            pageHandler: null,
+                            values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                            protocol: Request.Scheme);
 
+                        await emailSender.SendEmailAsync(Input.Email, "Bekräfta din e-post",
+                            $"Bekräfta ditt konto genom att <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl});
+                    }
+                    else
+                    {
+                        await signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
 
             // Fallback.
