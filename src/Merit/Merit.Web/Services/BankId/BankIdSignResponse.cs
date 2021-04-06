fde0c67d8da0b5d9550e5a7a3a1f7f94698ab612
Jimmy Borin <35033267+Xehlwan@users.noreply.github.com>
2021-04-06 15:11:15 +0200

    Refactoring and Error-handling
    Separated test-code from the rest of the classes.
    Added error-handling.
    Prepared for respone-messages to user.

--- src/Merit/Merit.Web/Services/BankId/BankIdSignResponse.cs
+++ src/Merit/Merit.Web/Services/BankId/BankIdSignResponse.cs
@@ -2,11 +2,13 @@
 
 namespace Merit.Web.Services.BankId
 {
-    public record BankIdSignResponse : BankIdResponse
+    public record BankIdSignResponse : IBankIdResponse
     {
         public Guid OrderRef { get; init; }
         public Guid AutoStartToken { get; init; }
         public Guid QrStartToken { get; init; }
         public Guid QrStartSecret { get; init; }
+
+        public MessageCode MessageCode => MessageCode.None;
     }
 }
--- src/Merit/Merit.Web/Services/BankId/BankIdTestService.cs
+++ src/Merit/Merit.Web/Services/BankId/BankIdTestService.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
 namespace Merit.Web.Services.BankId
 {
