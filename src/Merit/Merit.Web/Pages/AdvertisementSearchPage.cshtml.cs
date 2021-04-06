71beb7fcf89b04be4148f029973c187c5d147702
PivoKrtek <71064195+PivoKrtek@users.noreply.github.com>
2021-04-06 11:10:05 +0200

    Added advertisementsearchpage

---
+++ src/Merit/Merit.Web/Pages/AdvertisementSearchPage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Merit.Web.Pages
+{
+    public class AdvertisementSearchPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
