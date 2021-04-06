e40a79bd138441f76e3a27d918ea0469cf623165
PivoKrtek <71064195+PivoKrtek@users.noreply.github.com>
2021-04-06 11:11:50 +0200

    .

--- src/Merit/Merit.Web/Pages/AdvertisementSearchPage.cshtml.cs
+++ src/Merit/Merit.Web/Pages/AdvertisementSearchPage.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Merit.AdvertisementService;
+using Merit.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,56 @@ namespace Merit.Web.Pages
 {
     public class AdvertisementSearchPageModel : PageModel
     {
+        private readonly IAdvertisementService advertisementService = new AdvertisementService.AdvertisementService();
+
+        [BindProperty(SupportsGet = true)]
+        public string freeText { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int SearchType { get; set; }
+
+
+        public List<CompanyAdvertisement> resultSet { get; set; }
+
+
         public void OnGet()
         {
+            if (freeText != null)
+            {
+                if (SearchType == 1)
+                { resultSet = advertisementService.FreeSearchAdvertisements(freeText); }
+                else if (SearchType == 2)
+                {
+                    resultSet = advertisementService.FreeSearchMeritsInAdvertisements(freeText);
+                }
+                else if (SearchType == 3)
+                {
+                    resultSet = advertisementService.FreeSearchWantsInAdvertisements(freeText);
+                }
+
+            }
         }
+
+
+
+        public void OnPost()
+        {
+            switch (SearchType)
+            {
+                case 1:
+                    resultSet = advertisementService.FreeSearchAdvertisements(freeText);
+                    break;
+                case 2:
+                    resultSet = advertisementService.FreeSearchMeritsInAdvertisements(freeText);
+                    break;
+                case 3:
+                    resultSet = advertisementService.FreeSearchWantsInAdvertisements(freeText);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
     }
 }
