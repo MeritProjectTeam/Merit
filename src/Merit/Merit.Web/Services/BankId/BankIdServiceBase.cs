03296a73e26cc9bdce68b2cc6b868d3579a88021
Jimmy Borin <35033267+Xehlwan@users.noreply.github.com>
2021-04-06 13:08:02 +0200

    Created BankID service
    Created page at /bankid
    Page returns either an auth response or an errorcode in the body.

---
+++ src/Merit/Merit.Web/Services/BankId/BankIdServiceBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Merit.Web.Services.BankId
+{
+    public abstract class BankIdServiceBase
+    {
+        private static readonly MediaTypeHeaderValue jsonContentHeader = new MediaTypeHeaderValue(@"application/json");
+
+        private readonly X509Certificate2 localCert;
+        private readonly HttpClient httpClient;
+        private readonly HttpClientHandler clientHandler;
+
+        protected BankIdServiceBase(string certFile, string certPass, Uri baseAddress)
+        {
+            localCert = new X509Certificate2(certFile, certPass);
+
+            clientHandler = new();
+            clientHandler.ClientCertificates.Add(localCert);
+            clientHandler.ServerCertificateCustomValidationCallback = ValidateServerCertificate;
+
+            httpClient = new HttpClient(clientHandler);
+            httpClient.BaseAddress = baseAddress;
+            httpClient.DefaultRequestVersion = HttpVersion.Version11; // Default value, but made explicit for this API.
+        }
+
+        public async Task<BankIdResponse> BeginAuthorizeAsync(string personalNr, string userIp)
+        {
+            JsonContent content = JsonContent.Create(new
+            {
+                personalNumber = personalNr,
+                endUserIp = userIp
+            });
+            content.Headers.ContentType = jsonContentHeader;
+            HttpResponseMessage response = await httpClient.PostAsync("auth", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<BankIdSignResponse>();
+            }
+
+            return await response.Content.ReadFromJsonAsync<BankIdResponse>();
+        }
+
+        protected abstract bool ValidateServerCertificate(HttpRequestMessage requestMessage, X509Certificate2 cert, X509Chain chain, SslPolicyErrors errors);
+
+    }
+}
