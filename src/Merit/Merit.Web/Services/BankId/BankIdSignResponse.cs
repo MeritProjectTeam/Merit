d911f48b8c8c5d453ae7588511b67f8ae8054b6f
Jimmy Borin <35033267+Xehlwan@users.noreply.github.com>
2021-04-06 13:08:02 +0200

    Created BankID service
    Created page at /bankid
    Page returns either an auth response or an errorcode in the body.

---
+++ src/Merit/Merit.Web/Services/BankId/BankIdSignResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Merit.Web.Services.BankId
+{
+    public record BankIdSignResponse : BankIdResponse
+    {
+        public Guid OrderRef { get; init; }
+        public Guid AutoStartToken { get; init; }
+        public Guid QrStartToken { get; init; }
+        public Guid QrStartSecret { get; init; }
+    }
+}
